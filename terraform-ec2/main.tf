fbd7e32c5df9e5e78d80f80213c5ea03c21a5b5b
yaswanthreddypenchikala <yaswanthreddy877@gmail.com>
2026-08-22 10:25:14 +0000

    updated

--- terraform-ec2/main.tf
+++ terraform-ec2/main.tf
@@ -1,7 +1,34 @@
+
+
+# Create Security Group
+resource "aws_security_group" "terraform_all" {
+  name        = "terraform-all-sg"
+  description = "Allow all inbound and outbound traffic"
+
+  # Allow all inbound traffic
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
 resource "aws_instance" "terraform-example" {
   ami                    = "ami-0220d79f3f480ecf5"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-06768f041e8b8b319"]
+  vpc_security_group_ids = ["aws_security_group.terraform_all.id"]
 
   tags = {
     Name = "terraform-server"
